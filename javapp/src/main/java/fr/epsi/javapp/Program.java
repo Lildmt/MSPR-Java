acfedd29775c4ac69986415ef82b8faca623b165
Ni_mo <dem0nit3625@gmail.com>
2022-02-02 14:02:09 +0100

    Create Reader text

--- javapp/src/main/java/fr/epsi/javapp/Program.java
+++ javapp/src/main/java/fr/epsi/javapp/Program.java
@@ -3,10 +3,13 @@ import java.io.File;
 import java.util.Scanner;
 
 public class Program {
+
+    //Lecteur de fichier Texte 
     public static void main(String[] args) throws Exception
     {
         File doc = new File("C:\\Users\\tasoe\\Desktop\\doc.txt");
         Scanner obj = new Scanner(doc);
-        while (obj.hasNextLine())System.out.println(obj.nextLine());
+        while (obj.hasNextLine())
+        System.out.println(obj.nextLine());
     }
 }
